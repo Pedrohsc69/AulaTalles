c4c4e21af6f599acdf0a68e8fb6464f982c55da5
PEDRO <pedrocav646@gmail.com>
2024-08-22 17:20:47 -0300

    :tada: initial commit

---
+++ Calculator/src/FunCalculator.java
@@ -0,0 +1,20 @@
+public class FunCalculator {
+    double valorUM;
+    double valorDOIS;
+
+    double somar(double valorUM, double valorDOIS){
+        return valorUM + valorDOIS;
+    }
+
+    double subtrair(double valorUM, double valorDOIS){
+        return valorUM - valorDOIS;
+    }
+
+    double multi(double valorUM, double valorDOIS){
+        return valorUM * valorDOIS;
+    }
+
+    double division(double valorUM, double valorDOIS){
+        return valorUM / valorDOIS;
+    }
+}
---
+++ Calculator/src/Main.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        FunCalculator cal = new FunCalculator();
+
+        double numUM;
+        double numDOIS;
+        int opc;
+        do {
+            System.out.print("------------------");
+            System.out.println("\n1 - Somar");
+            System.out.println("2 - Subtrair");
+            System.out.println("3 - Multiplicar");
+            System.out.println("4 - Dividir");
+            System.out.println("5 - Sair");
+            System.out.println("------------------");
+            System.out.print("Escolha uma opção: ");
+            opc = ler.nextInt();
+
+            switch (opc){
+                case 1: /* SOMA */
+                    System.out.println("------------------");
+                    System.out.print("Digite um valor: ");
+                    numUM = ler.nextDouble();
+
+                    System.out.print("Digite outro valor: ");
+                    numDOIS = ler.nextDouble();
+
+                    double resultSoma = cal.somar(numUM,numDOIS);
+                    System.out.print(numUM+" + "+numDOIS+" = "+resultSoma+"\n");
+                    break;
+
+                case 2: /* SUBTRAÇÃO */
+                    System.out.println("------------------");
+                    System.out.print("Digite um valor: ");
+                    numUM = ler.nextDouble();
+
+                    System.out.print("Digite outro valor: ");
+                    numDOIS = ler.nextDouble();
+
+                    double resultSub = cal.subtrair(numUM,numDOIS);
+                    System.out.print(numUM+" - "+numDOIS+" = "+resultSub+"\n");
+                    break;
+
+                case 3: /* MULTIPLICAÇÃO */
+                    System.out.println("------------------");
+                    System.out.print("Digite um valor: ");
+                    numUM = ler.nextDouble();
+
+                    System.out.print("Digite outro valor: ");
+                    numDOIS = ler.nextDouble();
+
+                    double resultMulti = cal.multi(numUM,numDOIS);
+                    System.out.print(numUM+" * "+numDOIS+" = "+resultMulti+"\n");
+                    break;
+
+                case 4: /* DIVISÃO */
+                    System.out.println("------------------");
+                    System.out.print("Digite um valor: ");
+                    numUM = ler.nextDouble();
+
+                    System.out.print("Digite outro valor: ");
+                    numDOIS = ler.nextDouble();
+
+                    double resultDiv = cal.division(numUM,numDOIS);
+                    System.out.print(numUM+" / "+numDOIS+" = "+resultDiv+"\n");
+                    break;
+
+            }
+        } while (opc != 5);
+    }
+}
