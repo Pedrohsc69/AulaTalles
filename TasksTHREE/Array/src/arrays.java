9fba360e21a0f1558477f4d9a77918ee02c31793
PEDRO <pedrocav646@gmail.com>
2024-09-03 11:58:47 -0300

    The arrays need to be finished

---
+++ TasksTHREE/Array/src/arrays.java
@@ -0,0 +1,9 @@
+public class arrays {
+    public static void main(String[] args) {
+        String[] nomes = {"Alice", "Bob", "Carlos", "Diana",
+                "Eva", "Lucas", "Rani", "Piter", "Jhon", "Fabricio"};
+        for (int i = 3; i <= 10; i++) {
+            System.out.println(nomes[i]);
+        }
+    }
+}
