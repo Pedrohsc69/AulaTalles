08c75730437eff2862e6f95a64399986d684ec12
PEDRO <pedrocav646@gmail.com>
2024-09-06 19:49:11 -0300

    The arrays finished

--- TasksTHREE/Array/src/arrays.java
+++ TasksTHREE/Array/src/arrays.java
@@ -1,9 +1,39 @@
+import java.util.ArrayList;
+
 public class arrays {
     public static void main(String[] args) {
-        String[] nomes = {"Alice", "Bob", "Carlos", "Diana",
-                "Eva", "Lucas", "Rani", "Piter", "Jhon", "Fabricio"};
-        for (int i = 3; i <= 10; i++) {
-            System.out.println(nomes[i]);
+        int[] numeros = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        for (int i = 3; i <= 9; i++) {
+            System.out.print(numeros[i] + " ");
         }
+
+        System.out.print("\n----------------------------------------------\n");
+
+        ArrayList<String> nomes = new ArrayList<>();
+        nomes.add("Ana");
+        nomes.add("Bruno");
+        nomes.add("Carlos");
+        nomes.add("Diana");
+        nomes.add("Eduardo");
+
+
+        nomes.remove(1);
+
+
+        System.out.println("Após remover o segundo nome: " + nomes);
+
+
+        nomes.remove(2);
+
+
+        System.out.println("Após remover o terceiro nome: " + nomes);
+
+
+        if (nomes.get(0).equals("Talles")) {
+            System.out.println("O primeiro nome é Talles.");
+        } else {
+            System.out.println("O primeiro nome não é Talles.");
+        }
+
     }
 }
