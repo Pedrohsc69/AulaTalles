c096b985cb8f095c4d853d47e922e80e3d49d3a4
Pedro.hsc <114003818+Pedrohsc69@users.noreply.github.com>
2024-12-03 11:01:33 -0300

    Delete Java poo/Ifood/src/src directory

--- Java poo/Ifood/src/src/Status_Entrega.java
+++
@@ -1,7 +0,0 @@
-public class Status_Entrega extends Base{
-
-    private final String Producao = "Produção";
-    private final String A_Caminho = "A caminho";
-    private final String Entregue = "Entregue";
-
-}
